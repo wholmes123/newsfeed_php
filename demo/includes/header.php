4bde7a7b9846178f8929d24714be77a554a34df4
Chuan Wang <cw942078@Broadcom.net>
2019-02-06 17:12:05 -0800

    chapt6-commit1

---
+++ demo/includes/header.php
@@ -0,0 +1,10 @@
+<?php
+require 'config/config.php';
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+     <head>
+         <title>Welcome to NewsFeed!</title>
+     </head>
+     <body>
--- demo/index.php
+++ demo/index.php
@@ -1,23 +1,14 @@
 
 
 <?php
-$con = mysqli_connect("localhost", "root", "", "social");
-
-if (mysqli_connect_errno()) {
-    echo "Failed to connect: " . mysqli_connect_errno();
-}
-
-// $query = mysqli_query($con, "INSERT INTO test VALUES(NULL, 'mi')");
-
+include("includes/header.php");
  ?>
 
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-    <head>
-        <meta charset="utf-8">
-        <title> SwirlFeed</title>
-    </head>
-    <body>
-        Hello Chuan !!!!!!!!!
-    </body>
+
+    <div class="user_details column">
+        <a href="#"><img src="<?php echo $user['profile_pic']; ?>" alt=""> </a>
+
+    </div>
+
+</body>
 </html>
